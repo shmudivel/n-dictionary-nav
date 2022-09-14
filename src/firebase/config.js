5f9915d6d174fab2c1bd918842ec19073454fdce
shmudivel <82043832+shmudivel@users.noreply.github.com>
2022-09-14 17:40:36 -0400

    firebase starter

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,2 +1,19 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBKc1axUACVtbCl8BIHKqTjoh1uD3Me-Z4",
+    authDomain: "n-dictionary-nav.firebaseapp.com",
+    projectId: "n-dictionary-nav",
+    storageBucket: "n-dictionary-nav.appspot.com",
+    messagingSenderId: "952028519122",
+    appId: "1:952028519122:web:563325d5786f6d0009eb0b"
+  };
+
+  //init firebase 
+  firebase.initializeApp(firebaseConfig)
+
+  //init service 
+  const projectFirestore = firebase.firestore()
+
+  export{ projectFirestore }
